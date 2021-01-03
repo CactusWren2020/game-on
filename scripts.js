3b0af48d04b1056eccc1d0225108da6810b48f17
Mike Cho <elegantspanishguitar@gmail.com>
2021-01-03 09:56:02 -0700

    first draft

--- scripts.js
+++ scripts.js
@@ -1,3 +1,26 @@
+// scroll to top button
+
+//Get the button:
+mybutton = document.getElementById("scrollTop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+// mobile menu
 const mobileMenu = document.querySelector("#mobile-menu");
 const mobileButton = document.querySelector("#mobile-menu-button");
 const hamburgerIcon = document.querySelector("#hamburger-icon");
@@ -55,30 +78,56 @@ function showSlides(n) {
 
 //blog slider 
 
-var postIndex = 1;
-showPosts(postIndex);
+var postIndex = 0;
+showPosts(postIndex); //loads initial slides
 
-function plusPost(n) {
+function plusPost(n) {  
   showPosts((postIndex += n));
 }
 
-function currentPost(n) {
-  showPosts((postIndex = n));
-}
-
-function showPosts(n) {
-  var i;
+function showPosts(x) {
   var posts = document.getElementsByClassName("post");
-  if (n > posts.length) {
-    postIndex = 1;
+ 
+if (x < 0) { //sets postIndex to last item
+  postIndex = posts.length - 1;
+}
+if (x === posts.length) { //sets postIndex to first item
+  postIndex = 0;
+}
+//sets all slides to display none
+for (i = 0; i < posts.length; i++) {
+  posts[i].style.display = "none";
+}
+//display postIndex
+posts[postIndex].style.display = "flex";
+switch(postIndex) {
+  case 0:
+    posts[postIndex + 1].style.display = "flex";
+    posts[postIndex + 2].style.display = "flex";
+    break;
+  case 1:
+    posts[postIndex + 1].style.display = "flex";
+    posts[postIndex + 2].style.display = "flex";
+    break;
+  case 2: 
+    posts[postIndex + 1].style.display = "flex";
+    posts[postIndex + 2].style.display = "flex";
+    break;
+  case 3:
+    posts[postIndex + 1].style.display = "flex";
+    posts[postIndex + 2].style.display = "flex";
+    break;
+  case 4:
+    posts[postIndex + 1].style.display = "flex";
+    posts[0].style.display = "flex";
+    break;
+  case 5:
+    posts[0].style.display = "flex";
+    posts[1].style.display = "flex";
+    break;
+   
   }
-  if (n < 1) {
-    postIndex = posts.length;
-  }
-  for (i = 0; i < posts.length; i++) {
-    posts[i].style.display = "none";
-  }
-  posts[postIndex - 1].style.display = "flex";
+ 
 }
 
 //is-visible plugin
